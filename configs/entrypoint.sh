a58be8ebbf53241027870ee76ec73a17844ee954
godfuzz3r <godfuzz3r1337@gmail.com>
2025-02-11 14:52:29 +0300

    move to host network to get antsdr work

--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -80,11 +80,11 @@ osmo-bts-trx -c /etc/osmocom/osmo-bts.cfg &
 
 if [[ $(yq -rM '.egprs."routing-enabled"' /configs/config.yml) = "true" ]]; then
     # get output interface name
-    iface=$(ip route show default | awk -F'dev ' '{ print $2 }')
+    iface=$(ip route show default | awk -F'dev ' '{ print $2 }' | awk '{ print $1 }')
 
     iptables -A FORWARD -i apn0 -o $iface -j ACCEPT
     iptables -A FORWARD -i $iface -o apn0 -j ACCEPT
-    iptables -t nat -A POSTROUTING -o $iface -j SNAT --to-source 172.16.80.10
+    iptables -t nat -A POSTROUTING -o $iface -j MASQUERADE
 
     # TODO: ipv6 routing to internet
 fi

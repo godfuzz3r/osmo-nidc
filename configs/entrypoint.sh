d67536979986c187513f46799215222cf10a5253
godfuzz3r <godfuzz3r1337@gmail.com>
2025-08-06 21:35:16 +0300

    add proxy and routing related configurations

--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+
+cleanup() {
+    echo "Container stopped, performing iptables cleanup..."
+    iface=$(yq -rM '.egprs."output-interface"' /configs/config.yml)
+    ip_prefix=$(yq -rM '.egprs."ip-prefix"' /configs/config.yml)
+
+    iptables -D FORWARD -i apn0 -o "$iface" -j ACCEPT 2>/dev/null
+    iptables -D FORWARD -i "$iface" -o apn0 -j ACCEPT 2>/dev/null
+    iptables -t nat -D POSTROUTING -s "$ip_prefix" -o "$iface" -j MASQUERADE 2>/dev/null
+
+    iptables -t nat -D PREROUTING -i apn0 -p tcp -j REDIRECT --to-ports 12346 2>/dev/null
+}
+trap 'cleanup' SIGTERM
+
+# first we need determine which iptables used on the host: legacy or nf_tables
+# taken from kubernetes https://github.com/kubernetes/kubernetes/blob/ffe93b3979486feb41a0f85191bdd189cbd56ccc/build/debian-iptables/iptables-wrapper
+num_legacy_lines=$( (iptables-legacy-save || true; ip6tables-legacy-save || true) 2>/dev/null | grep '^-' | wc -l)
+if [ "${num_legacy_lines}" -ge 10 ]; then
+    mode=legacy
+else
+    num_nft_lines=$( (timeout 5 sh -c "iptables-nft-save; ip6tables-nft-save" || true) 2>/dev/null | grep '^-' | wc -l)
+    if [ "${num_legacy_lines}" -ge "${num_nft_lines}" ]; then
+    mode=legacy
+    else
+    mode=nft
+    fi
+fi
+
+update-alternatives --set iptables "/usr/sbin/iptables-${mode}" > /dev/null
+update-alternatives --set ip6tables "/usr/sbin/ip6tables-${mode}" > /dev/null
+
 ### setup configs
 cp /configs/osmocom/osmo-*.cfg /etc/osmocom/
 
@@ -78,14 +109,31 @@ sleep 5
 osmo-bts-trx -c /etc/osmocom/osmo-bts.cfg &
 
 
+if [[ $(yq -rM '.egprs."proxy-enabled"' /configs/config.yml) = "true" ]]; then
+    iptables -t nat -D PREROUTING -i apn0 -p tcp -j REDIRECT --to-ports 12346 2>/dev/null
+    iptables -t nat -A PREROUTING -i apn0 -p tcp -j REDIRECT --to-ports 12346
 
-if [[ $(yq -rM '.egprs."routing-enabled"' /configs/config.yml) = "true" ]]; then
-    # get output interface name
-    iface=$(ip route show default | awk -F'dev ' '{ print $2 }')
+    cp /configs/redsocks/redsocks.conf /etc/redsocks.conf
+    local_ip=$(yq -rM '.egprs."ip-ifconfig"' /configs/config.yml | awk -F'/' '{ print $1 }')
+    sed "s@#local_ip#@$local_ip@g" -i /etc/redsocks.conf
+    redsocks -c /etc/redsocks.conf
 
-    iptables -A FORWARD -i apn0 -o $iface -j ACCEPT
-    iptables -A FORWARD -i $iface -o apn0 -j ACCEPT
-    iptables -t nat -A POSTROUTING -o $iface -j SNAT --to-source 172.16.80.10
+    proxy_url=$(yq -rM '.egprs."proxy-url"' /configs/config.yml)
+    ./gost -L 127.0.0.1:1088 -F "$proxy_url" &
+    # TODO: ipv6 routing to internet
+elif [[ $(yq -rM '.egprs."routing-enabled"' /configs/config.yml) = "true" ]]; then
+    sysctl -w net.ipv4.ip_forward=1
+
+    iface=$(yq -rM '.egprs."output-interface"' /configs/config.yml)
+    ip_prefix=$(yq -rM '.egprs."ip-prefix"' /configs/config.yml)
+
+    iptables -D FORWARD -i apn0 -o "$iface" -j ACCEPT 2>/dev/null
+    iptables -D FORWARD -i "$iface" -o apn0 -j ACCEPT 2>/dev/null
+    iptables -t nat -D POSTROUTING -s "$ip_prefix" -o "$iface" -j MASQUERADE 2>/dev/null
+
+    iptables -A FORWARD -i apn0 -o "$iface" -j ACCEPT
+    iptables -A FORWARD -i "$iface" -o apn0 -j ACCEPT
+    iptables -t nat -A POSTROUTING -s "$ip_prefix" -o "$iface" -j MASQUERADE
 
     # TODO: ipv6 routing to internet
 fi
@@ -96,4 +144,7 @@ nice -n 0 dnsmasq -C /configs/dnsmasq/sgsn.conf
 # dns for devices that uses APN
 nice -n 0 dnsmasq -C /configs/dnsmasq/apn0.conf
 
-nice -n 19 sleep infinity
+nice -n 19 sleep infinity &
+
+wait $!
+cleanup
